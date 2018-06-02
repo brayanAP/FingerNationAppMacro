d02572b5bd8ea3678668af8bb14691ac167087fc
brayanAP <brulariaspe@gmail.com>
2018-06-02 15:31:12 -0500

    brayanAP

---
+++ WcfService/ServiceFingerNation.svc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServiceFingerNation" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServiceFingerNation.svc o ServiceFingerNation.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServiceFingerNation : IServiceFingerNation
+    {
+        /*AQUI VA LA LOGICA PARA GUARDAR EN SQL SERVER*/
+    }
+}
